ddc1969458c3636e5698fa2bc25efe92a7748954
Avneendra Kanva <avneendrakanva@gmail.com>
2020-05-05 01:00:47 -0700

    Project 2 Submission

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -28,7 +28,23 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
   /**************************************************************************** */
-
+  app.get("/filteredimage/", async ( req, res ) =>  {
+    let { image_url } = req.query;
+    if (!image_url) {
+        return res.status(400)
+            .send(`URL is required`);
+    }
+    let filteredpath : string;
+    try {
+        filteredpath = await filterImageFromURL(image_url);
+    }
+    catch (e) {
+        res.status(500).send(`Internal Server Error! Error in filtering image from URL`);
+    }
+    res.sendfile(filteredpath, function () {
+        deleteLocalFiles(filteredpath);
+    });
+});
   //! END @TODO1
   
   // Root Endpoint
